814b2c42b2fde423a2cf8742302d5626b2512f7e
StackiCode <stacki.code@gmail.com>
2020-07-04 22:00:34 +0700

    Updates

---
+++ precache-manifest.8b7097d36e640eae0d415599eae769d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b06743d7e5ce0461ce1a7755875312",
+    "url": "/spinner-appear/index.html"
+  },
+  {
+    "revision": "db14f0ca48b9a62a4e47",
+    "url": "/spinner-appear/static/css/main.961cf63c.chunk.css"
+  },
+  {
+    "revision": "57fdde1875ba36c72889",
+    "url": "/spinner-appear/static/js/2.17db939c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spinner-appear/static/js/2.17db939c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db14f0ca48b9a62a4e47",
+    "url": "/spinner-appear/static/js/main.ed64acd3.chunk.js"
+  },
+  {
+    "revision": "9caa0a9de8eaa0185844",
+    "url": "/spinner-appear/static/js/runtime-main.39f42c4e.js"
+  }
+]);
